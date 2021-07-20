9fc91ee764b1a725fa5730e2fda750c6c02f7a1f
toshofan <tramptrump@gmail.com>
2021-07-20 20:38:47 +0300

    first commit

---
+++ extract-files.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017, 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+# Required!
+DEVICE=vince
+VENDOR=xiaomi
+
+# Load extract_utils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
+
+LINEAGE_ROOT="${MY_DIR}/../../.."
+
+HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Default to sanitizing the vendor folder before extraction
+CLEAN_VENDOR=true
+
+SECTION=
+KANG=
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
+    esac
+    shift
+done
+
+if [ -z "${SRC}" ]; then
+    SRC="adb"
+fi
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+
+extract "${MY_DIR}"/proprietary-files.txt "${SRC}" \
+        "${KANG}" --section "${SECTION}"
+
+DEVICE_BLOB_ROOT="${LINEAGE_ROOT}"/vendor/"${VENDOR}"/"${DEVICE}"/proprietary
+
+# Camera configs
+sed -i "s|/system/etc/camera|/vendor/etc/camera|g" "${DEVICE_BLOB_ROOT}"/vendor/lib/libmmcamera2_sensor_modules.so
+
+# Camera socket
+sed -i "s|/data/misc/camera/cam_socket|/data/vendor/qcam/cam_socket|g" "${DEVICE_BLOB_ROOT}"/vendor/bin/mm-qcamera-daemon
+
+# Camera data
+for CAMERA_LIB in libmmcamera2_cpp_module.so libmmcamera2_dcrf.so libmmcamera2_iface_modules.so libmmcamera2_imglib_modules.so libmmcamera2_mct.so libmmcamera2_pproc_modules.so libmmcamera2_q3a_core.so libmmcamera2_sensor_modules.so libmmcamera2_stats_algorithm.so libmmcamera2_stats_modules.so libmmcamera_dbg.so libmmcamera_imglib.so libmmcamera_pdafcamif.so libmmcamera_pdaf.so libmmcamera_tintless_algo.so libmmcamera_tintless_bg_pca_algo.so libmmcamera_tuning.so; do
+    sed -i "s|/data/misc/camera/|/data/vendor/qcam/|g" "${DEVICE_BLOB_ROOT}"/vendor/lib/${CAMERA_LIB}
+done
+
+# Camera debug log file
+sed -i "s|persist.camera.debug.logfile|persist.vendor.camera.dbglog|g" "${DEVICE_BLOB_ROOT}"/vendor/lib/libmmcamera_dbg.so
+
+"${MY_DIR}/setup-makefiles.sh"
---
+++ prop.mk
@@ -0,0 +1,263 @@
+#
+# system.prop for vince
+#
+
+# ART
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.dex2oat-filter=speed \
+dalvik.vm.image-dex2oat-filter=speed
+
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+af.fast_track_multiplier=2 \
+audio.offload.disable=true \
+audio.offload.min.duration.secs=30 \
+audio.offload.video=true \
+persist.vendor.audio.fluence.speaker=true \
+persist.vendor.audio.fluence.voicecall=true \
+persist.vendor.audio.fluence.voicerec=false \
+persist.vendor.audio.hw.binder.size_kbyte=1024 \
+persist.vendor.audio.hw.binder.size_kbyte=1024 \
+persist.vendor.btstack.enable.splita2dp=false \
+ro.config.media_vol_steps=25 \
+ro.config.vc_call_vol_steps=7 \
+ro.vendor.audio.sdk.fluencetype=fluence \
+ro.vendor.audio.sdk.ssr=false \
+vendor.audio.flac.sw.decoder.24bit=true \
+vendor.audio.offload.buffer.size.kb=64 \
+vendor.audio.offload.gapless.enabled=true \
+vendor.audio.offload.multiaac.enable=true \
+vendor.audio.offload.multiple.enabled=false \
+vendor.audio.offload.track.enable=true \
+vendor.audio.parser.ip.buffer.size=0 \
+vendor.audio.playback.mch.downsample=true \
+vendor.audio.pp.asphere.enabled=false \
+vendor.audio.safx.pbe.enabled=true \
+vendor.audio.tunnel.encode=false \
+vendor.audio.use.sw.alac.decoder=true \
+vendor.audio.use.sw.ape.decoder=true \
+vendor.audio_hal.period_size=192 \
+vendor.voice.conc.fallbackpath=deep-buffer \
+vendor.voice.path.for.pcm.voip=true \
+vendor.voice.playback.conc.disabled=true \
+vendor.voice.record.conc.disabled=false \
+vendor.voice.voip.conc.disabled=true
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+bluetooth.hfp.client=1 \
+vendor.qcom.bluetooth.soc=smd \
+ro.bluetooth.hfp.ver=1.7
+
+# CAF props
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.memperf.enable=false \
+ro.memperf.lib=libmemperf.so \
+ro.vendor.qti.config.zram=true \
+ro.vendor.qti.sys.fw.empty_app_percent=50 \
+ro.vendor.qti.sys.fw.trim_cache_percent=100 \
+ro.vendor.qti.sys.fw.trim_empty_percent=100 \
+ro.vendor.qti.sys.fw.trim_enable_memory=2147483648 \
+ro.vendor.qti.sys.fw.use_trim_settings=true \
+vendor.vidc.enc.disable.pq=true
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+camera.display.lmax=1280x720 \
+camera.display.umax=1920x1080 \
+vendor.camera.aux.packagelist=org.lineageos.snap,com.google.android.GoogleCamera \
+vendor.camera.hal1.packagelist=com.whatsapp,com.facebook.katana,com.instagram.android,com.snapchat.android \
+camera.lowpower.record.enable=1 \
+media.camera.ts.monotonic=1 \
+persist.camera.gyro.disable=0 \
+persist.camera.isp.clock.optmz=0 \
+persist.camera.stats.test=5 \
+persist.vendor.qti.telephony.vt_cam_interface=2 \
+vidc.enc.dcvs.extra-buff-count=2 \
+persist.camera.HAL3.enabled=1 \
+persist.camera.eis.enable=1 \
+persist.camera.CDS=off
+
+# Cne
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.cne.feature=1
+
+# Coresight
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.debug.coresight.config=stm-events
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.composition.type=c2d \
+debug.cpurend.vsync=false \
+debug.egl.hw=1 \
+debug.enable.sglscale=1 \
+debug.gralloc.enable_fb_ubwc=1 \
+debug.hwui.renderer=skiagl \
+debug.mdpcomp.idletime=600 \
+debug.mdpcomp.logs=0 \
+debug.performance.tuning=1 \
+debug.sf.disable_backpressure=1 \
+debug.sf.disable_hwc=0 \
+debug.sf.enable_hwc_vds=1 \
+debug.sf.gpu_comp_tiling=1 \
+debug.sf.hw=1 \
+debug.sf.latch_unsignaled=1 \
+debug.sf.recomputecrop=0 \
+dev.pm.dyn_samplingrate=1 \
+persist.demo.hdmirotationlock=false \
+persist.hwc.enable_vds=1 \
+persist.hwc.mdpcomp.enable=true \
+persist.hwc.ptor.enable=true \
+ro.opengles.version=196610 \
+ro.qualcomm.cabl=0 \
+ro.vendor.display.cabl=2 \
+sdm.debug.disable_skip_validate=1 \
+vendor.display.disable_skip_validate=1 \
+vendor.display.enable_default_color_mode=1 \
+vendor.gralloc.enable_fb_ubwc=1 \
+video.accelerate.hw=1
+
+# DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.dpm.feature=0
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+drm.service.enabled=true
+
+# Fingerprint
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.qfp=false
+
+# Fm
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.fm.transmitter=false \
+vendor.hw.fm.init=0
+
+# Frp
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.frp.pst=/dev/block/bootdevice/by-name/config
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.sf.early_phase_offset_ns=1500000 \
+debug.sf.early_app_phase_offset_ns=1500000 \
+debug.sf.early_gl_phase_offset_ns=3000000 \
+debug.sf.early_gl_app_phase_offset_ns=15000000
+
+# HWUI properties
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.hwui.texture_cache_size=72 \
+ro.hwui.layer_cache_size=48 \
+ro.hwui.r_buffer_cache_size=8 \
+ro.hwui.path_cache_size=32 \
+ro.hwui.gradient_cache_size=1 \
+ro.hwui.drop_shadow_cache_size=6 \
+ro.hwui.texture_cache_flushrate=0.4 \
+ro.hwui.text_small_cache_width=1024 \
+ro.hwui.text_small_cache_height=1024 \
+ro.hwui.text_large_cache_width=2048 \
+ro.hwui.text_large_cache_height=1024
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+av.debug.disable.pers.cache=1 \
+debug.stagefright.omx_default_rank.sw-audio=1 \
+debug.stagefright.omx_default_rank=0 \
+media.msm8956hw=0 \
+media.stagefright.audio.sink=280 \
+media.stagefright.thumbnail.prefer_hw_codecs=true \
+mmp.enable.3g2=true \
+vendor.audio.hw.aac.encoder=true \
+vendor.mm.enable.qcom_parser=1048575 \
+vendor.vidc.dec.downscalar_height=1088 \
+vendor.vidc.dec.downscalar_width=1920 \
+vendor.vidc.disable.split.mode=1 \
+vendor.vidc.enc.disable.pq=true \
+vendor.vidc.enc.disable_bframes=1 \
+vendor.video.disable.ubwc=1
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.sys.fw.dex2oat_thread_count=8 \
+ro.vendor.extension_library=libqti-perfd-client.so
+
+# Netmgrd
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.vendor.use_data_netmgrd=true \
+persist.data.netmgrd.qos.enable=true \
+persist.vendor.data.mode=concurrent
+
+# Nitz
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.radio.nitz_plmn="" \
+persist.vendor.radio.nitz_lons_0="" \
+persist.vendor.radio.nitz_lons_1="" \
+persist.vendor.radio.nitz_lons_2="" \
+persist.vendor.radio.nitz_lons_3="" \
+persist.vendor.radio.nitz_sons_0="" \
+persist.vendor.radio.nitz_sons_1="" \
+persist.vendor.radio.nitz_sons_2="" \
+persist.vendor.radio.nitz_sons_3=""
+
+# Qualcomm
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.vendor.qti.va_aosp.support=1 \
+ro.vendor.qti.va_odm.support=1
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+DEVICE_PROVISIONED=1 \
+persist.dbg.volte_avail_ovr=1 \
+persist.dbg.vt_avail_ovr=1 \
+persist.dbg.wfc_avail_ovr=1 \
+persist.radio.multisim.config=dsds \
+persist.sys.fflag.override.settings_network_and_internet_v2=true \
+persist.vendor.radio.apm_sim_not_pwdn=1 \
+persist.vendor.radio.custom_ecc=1 \
+persist.vendor.radio.hw_mbn_update=0 \
+persist.vendor.radio.rat_on=combine \
+persist.vendor.radio.sib16_support=1 \
+ril.subscription.types=NV,RUIM \
+rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+ro.telephony.iwlan_operation_mode=legacy \
+ro.telephony.call_ring.multiple=false \
+ro.telephony.default_network=22,22 \
+ro.telephony.use_old_mnc_mcc_format=true \
+service.qti.ims.enabled=1 \
+vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
+
+# SurfaceFlinger
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.sf.early_phase_offset_ns=1500000 \
+debug.sf.early_app_phase_offset_ns=1500000 \
+debug.sf.early_gl_phase_offset_ns=3000000 \
+debug.sf.early_gl_app_phase_offset_ns=15000000 \
+ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+ro.surface_flinger.max_virtual_display_dimension=4096
+
+# Time Services
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.delta_time.enable=true
+
+# Tcp
+PRODUCT_PROPERTY_OVERRIDES += \
+net.tcp.2g_init_rwnd=10
+
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+sys.use_fifo_ui=1
+
+# Usb
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.usb.config.extra=none
+
+# Wifi
+PRODUCT_PROPERTY_OVERRIDES += \
+wifi.interface=wlan0
+
+# Wifi-Display
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.sys.wfd.virtual=0
